7feb4f4304ec93fc9f45f90fd5e0599a9b7011b1
Gerd Rausch <gerd.rausch@oracle.com>
2019-02-06 17:55:37 -0800

    net/rds: Keep track of and wait for FRWR segments in use upon shutdown
    Since "rds_ib_free_frmr" and "rds_ib_free_frmr_list" simply put
    the FRMR memory segments on the "drop_list" or "free_list",
    and it is the job of "rds_ib_flush_mr_pool" to reap those entries
    by ultimately issuing a "IB_WR_LOCAL_INV" work-request,
    we need to trigger and then wait for all those memory segments
    attached to a particular connection to be fully released before
    we can move on to release the QP, CQ, etc.
    
    So we make "rds_ib_conn_path_shutdown" wait for one more
    atomic_t called "i_fastreg_inuse_count" that keeps track of how
    many FRWR memory segments are out there marked "FRMR_IS_INUSE"
    (and also wake_up rds_ib_ring_empty_wait, as they go away).
    
    Orabug: 29298000
    
    Change-Id: I2d0f19e2a5e41da104873397ed1fa5e64efb1d4f
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>

--- net/rds/ib.h
+++ net/rds/ib.h
@@ -154,6 +154,7 @@ struct rds_ib_connection {
 
 	/* To control the number of wrs from fastreg */
 	atomic_t		i_fastreg_wrs;
+	atomic_t		i_fastreg_inuse_count;
 
 	/* interrupt handling */
 	struct tasklet_struct	i_send_tasklet;
